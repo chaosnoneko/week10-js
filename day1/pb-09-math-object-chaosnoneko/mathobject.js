4bed6da130ab1850a8a3bd55eaa0ed3e670b05aa
chaosnoneko <98882297+chaosnoneko@users.noreply.github.com>
2022-04-25 14:50:29 +0200

    Add files via upload

---
+++ day1/pb-09-math-object-chaosnoneko/mathobject.js
@@ -0,0 +1,13 @@
+console.log(Math.min (-1 , 4 ));
+console.log(Math.max (-1 , 4 ));
+console.log(Math.ceil(3321.32321)) ;
+console.log(Math.ceil(326.76)) ;
+console.log(Math.ceil(76788.7)) ;
+console.log(Math.ceil(-9.78)) ;
+console.log(Math.ceil(43.342)) ;
+console.log(Math.round(3321.32321)) ;
+console.log(Math.round(326.76)) ;
+console.log(Math.round(76788.7)) ;
+console.log(Math.round(-9.78)) ;
+console.log(Math.round(28.329)) ;
+console.log(Math.random()*6)
---
+++ day1/pb-10-operators-arithmetic-2-chaosnoneko/operators.js
@@ -0,0 +1,23 @@
+let snackServing = 5 ;
+let guest = 7 ;
+console.log(snackServing + guest);
+console.log(guest - snackServing);
+console.log(snackServing - guest);
+console.log(snackServing*guest);
+console.log(snackServing/guest);
+let drink = "10"
+resultOne = (snackServing*guest)/ drink;
+console.log(resultOne)
+let monkey = 15 ;
+let bananas = 9 ;
+console.log(monkey/bananas);
+let monkeyfights = 20 ;
+resultTwo = (monkey + bananas) * monkeyfights;
+console.log(resultTwo);
+monkey++
+console.log(bananas);
+bananas--
+console.log(bananas);
+happiness = bananas - monkey ;
+console.log(happiness+monkeyfights);
+console.log(resultOne%resultTwo);
